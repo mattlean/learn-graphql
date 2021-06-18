ead614646fc92c089fd9d0203e2baa86cc17d610
Matthew Lean <matt@mattlean.com>
2021-06-17 17:52:06 -0700

    Add Relay implementation to fullstack

--- fullstack/hackernews/hackernews/schema.py
+++ fullstack/hackernews/hackernews/schema.py
@@ -2,12 +2,23 @@ import graphene
 import graphql_jwt
 
 import links.schema
+import links.schema_relay
 import users.schema
 
-class Query(users.schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    users.schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType
+):
     pass
 
-class Mutation(users.schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(
+    users.schema.Mutation,
+    links.schema.Mutation,
+    links.schema_relay.RelayMutation,
+    graphene.ObjectType
+):
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
